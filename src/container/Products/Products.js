37c71b4720978f8698e6b2a3118ccb25dc01e113
Suranjan <suranjanbiswas2002@gmail.com>
2021-03-10 22:14:12 +0530

    Created the basic folder setup

---
+++ src/container/Products/Products.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+import styles from './Products.module.css';
+
+class Products extends PureComponent {
+  render() {
+    return <h1>This is the product item page</h1>;
+  }
+}
+
+export default Products;
