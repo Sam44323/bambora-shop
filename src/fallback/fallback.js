968e4706f0ec9b4f6ce3bdfd5f2f9ffeae126d52
Suranjan Dutta Moitra <suranjanbiswas2002@gmail.com>
2021-03-21 19:49:06 +0530

    Added the a folder for fallback component

--- src/fallback/fallback.js
+++ src/fallback/fallback.js
@@ -1,10 +1,14 @@
 import {Fragment} from 'react';
+import Loader from 'react-loader-spinner';
 
 import Navigation from '../components/Navigation/Navigation';
+import sharedStyles from '../container/shared/styles.module.css';
 
 const Fallback = () => <Fragment>
   <Navigation/>
-  <h1>Loading...</h1>
+  <div className={sharedStyles.loadingSection}>
+    <Loader type="Circles" height={80} width={80} color="salmon"/>
+  </div>
 </Fragment>
 
 export default Fallback;
