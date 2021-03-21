e40ab2c3b73b45176e056db6dbb6737798143da0
Suranjan Dutta Moitra <suranjanbiswas2002@gmail.com>
2021-03-21 22:05:22 +0530

    Fixed a bug in the tokenChecker file

--- src/components/utils/tokenChecker.js
+++ src/components/utils/tokenChecker.js
@@ -1,10 +1,13 @@
 const checkValidation = () => {
   let isValid = false;
-  if(localStorage.getItem('token') && new Date(localStorage.getItem('expiresIn') > new Date()))
-  {
+  if (
+    localStorage.getItem("token") &&
+    new Date(localStorage.getItem("expiresIn")) > new Date()
+  ) {
+    console.log(new Date(localStorage.getItem("expiresIn")));
     isValid = true;
   }
   return isValid;
-}
+};
 
 export default checkValidation;
--- src/container/auth/Login/Login.js
+++ src/container/auth/Login/Login.js
@@ -1,32 +1,32 @@
-import { useCallback, useState, Fragment } from 'react';
+import { useCallback, useState, Fragment } from "react";
 
-import sharedStyles from '../../shared/styles.module.css';
-import Navigation from '../../../components/Navigation/Navigation';
-import btnStyles from '../../../components/Button/Button.module.css';
-import InputElement from '../../../components/InputElement/InputElement';
-import Button from '../../../components/Button/Button';
-import axios from 'axios';
+import sharedStyles from "../../shared/styles.module.css";
+import Navigation from "../../../components/Navigation/Navigation";
+import btnStyles from "../../../components/Button/Button.module.css";
+import InputElement from "../../../components/InputElement/InputElement";
+import Button from "../../../components/Button/Button";
+import axios from "axios";
 
 //util functions
-import { checkValidity } from '../../../components/utils/checkValidity';
+import { checkValidity } from "../../../components/utils/checkValidity";
 
 const Login = (props) => {
   const [formState, setFormState] = useState([
     {
-      name: 'email',
-      type: 'email',
+      name: "email",
+      type: "email",
       isValid: false,
       touched: false,
-      value: '',
-      message: 'Please enter your email!',
+      value: "",
+      message: "Please enter your email!",
     },
     {
-      name: 'password',
-      type: 'password',
+      name: "password",
+      type: "password",
       isValid: false,
       touched: false,
-      value: '',
-      message: 'Please enter a password of atlest 5 characters!',
+      value: "",
+      message: "Please enter a password of atlest 5 characters!",
     },
   ]);
 
@@ -49,41 +49,43 @@ const Login = (props) => {
       data[item.name] = item.value;
     }
     axios
-      .post('http://localhost:5000/bambora-shop/users/login-user', data)
+      .post("http://localhost:5000/bambora-shop/users/login-user", data)
       .then((response) => {
-        if(response){
-          const expiresTime= new Date();
+        if (response) {
+          const expiresTime = new Date();
           expiresTime.setHours(expiresTime.getHours() + 1);
-          localStorage.setItem('token', response.data.token);
-          localStorage.setItem('userId', response.data.userId);
-          localStorage.setItem('expiresIn', expiresTime.toISOString());
-          props.history.replace('/');
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("userId", response.data.userId);
+          localStorage.setItem("expiresIn", expiresTime.toISOString());
+          props.history.replace("/");
         }
       })
       .catch((err) => console.log(err));
-  }, [props,formState]);
+  }, [props, formState]);
 
-  return <Fragment>
-  <Navigation/>
-    <div className={sharedStyles.FormSection}>
-      <h1 className={sharedStyles.FormTitle}>Login</h1>
-      {formState.map((item) => (
-        <InputElement
-          key={item.name}
-          name={item.name}
-          type={item.type}
-          value={item.value}
-          changeAction={(name, value, type) => changeValue(name, value, type)}
-          isValid={item.isValid}
-          touched={item.touched}
-          invalidMessage={item.message}
-        />
-      ))}
-      <Button class={btnStyles.SuccessBtn} clickAction={submitForm}>
-        Login
-      </Button>
-    </div>
- </Fragment>
+  return (
+    <Fragment>
+      <Navigation />
+      <div className={sharedStyles.FormSection}>
+        <h1 className={sharedStyles.FormTitle}>Login</h1>
+        {formState.map((item) => (
+          <InputElement
+            key={item.name}
+            name={item.name}
+            type={item.type}
+            value={item.value}
+            changeAction={(name, value, type) => changeValue(name, value, type)}
+            isValid={item.isValid}
+            touched={item.touched}
+            invalidMessage={item.message}
+          />
+        ))}
+        <Button class={btnStyles.SuccessBtn} clickAction={submitForm}>
+          Login
+        </Button>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Login;
